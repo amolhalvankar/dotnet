6ed9d397ad29ba0740c7a4dd8d425fa5a989cdc2
amolhalvankar <amolhalvankar18@gmail.com>
2024-02-16 17:13:37 +0530

    Practice Session 1

---
+++ 1_Csharp Projects/First/First.cs
@@ -0,0 +1,8 @@
+﻿internal class First
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This is first program in practice eraa...");
+        Console.ReadLine();
+    }
+}
